dc7e360b343c4b16d3a25f75f85c66edde17b932
Aidan C <84241049+AidanCurley@users.noreply.github.com>
2021-10-17 16:32:32 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -9,9 +9,11 @@ DROP TABLE IF EXISTS card;
 DROP TABLE IF EXISTS paypal;
 
 CREATE TABLE card(
-  number CHAR(16) PRIMARY KEY,
-  sort_code CHAR(6),
-  cvv CHAR(3)
+  number CHAR(16) PRIMARY KEY NOT NULL,
+  sort_code CHAR(6) NOT NULL,
+  type VARCHAR(16) NOT NULL,
+  expiry_date DATE NOT NULL,
+  cvv CHAR(3) NOT NULL
 );
 
 CREATE TABLE paypal(
@@ -48,8 +50,10 @@ CREATE TABLE storefront (
 CREATE TABLE catalogue(
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
   product_id INT,
+  seller_id INT,
   price FLOAT(6,2),
-  FOREIGN KEY (product_id) REFERENCES product(id)
+  FOREIGN KEY (product_id) REFERENCES product(id),
+  FOREIGN KEY (seller_id) REFERENCES seller(id)
 );
 
 CREATE TABLE seller (
@@ -61,31 +65,23 @@ CREATE TABLE seller (
   address VARCHAR(50) NOT NULL,
   email VARCHAR(20) NOT NULL,
   phone CHAR(11),
-  storefront_id INTEGER NOT NULL,
-  catalogue_id INTEGER NOT NULL,
-  FOREIGN KEY (catalogue_id) REFERENCES catalogue(id),
-  FOREIGN KEY (storefront_id) REFERENCES storefront(id)
+  storefront_id INTEGER NOT NULL
 );
 
 
-INSERT INTO `customer` VALUES ('1','cust1','123456','John','Johnson','1 Carr St., Leeds, UK','john@hotmail.com','07912312312','1234123412341234','1111'),
-  ('2','cust2','123456','James','Jameson','2 Spitfire St., Leeds, UK','james@hotmail.com','07912312312','5678567856785678','2222'),
-  ('3','cust3','123456','Mike','Mitchell','22 Slatchett Row, Liverpool, UK','mike@hotmail.com','07999999999','1000100010001000','2222');
-INSERT INTO `seller` VALUES ('1','sell1','123456','Billy','Bobson','12 Comercial St., London, UK','bb@hotmail.com','07912312312','1','1'),
-  ('2','sell2','123456','Sarah','Smith','12 Spartan Ave., London, UK','sarah@hotmail.com','07912312312','2','2');
-INSERT INTO `storefront` VALUES ('1','logo1'),
-  ('2','logo2');
-INSERT INTO `catalogue` VALUES ('1','1','5.99'),
-  ('2','2','4.99'),
-  ('3','3','5.99'),
-  ('4','2','0.99'),
-  ('5','5','10.99'),
-  ('6','6','6.99'),
-  ('7','7','2.99');
-INSERT INTO `product` VALUES ('1','dog biscuits', '15'),('2','flea powder', '20'),('3','collar', '20'),
-  ('4','nail file', '20'),('5','vitamins', '10'),('6','shampoo', '20'),('7','tick remover', '20');
-INSERT INTO `card` VALUES ('1234123412341234','302010','123'),
-  ('5678567856785678','112233','999'),
-  ('1000100010001000','110033','555');
-INSERT INTO `paypal` VALUES ('1111','1000.00'),
-  ('2222','1000.00');
+INSERT INTO `customer` VALUES (1,'cust1','123456','John','Johnson','1 Carr St., Leeds, UK','john@hotmail.com','07912312312','1234123412341234','1111'),
+  (2,'cust2','123456','James','Jameson','2 Spitfire St., Leeds, UK','james@hotmail.com','07912312312','5678567856785678','2222'),
+  (3,'cust3','123456','Mike','Mitchell','22 Slatchett Row, Liverpool, UK','mike@hotmail.com','07999999999','1000100010001000','2222');
+INSERT INTO `seller` VALUES (1,'sell1','123456','Billy','Bobson','12 Comercial St., London, UK','bb@hotmail.com','07912312312',1),
+  (2,'sell2','123456','Sarah','Smith','12 Spartan Ave., London, UK','sarah@hotmail.com','07912312312',2);
+INSERT INTO `storefront` VALUES (1,'logo1'),
+  (2,'logo2');
+INSERT INTO `catalogue` (product_id, seller_id, price) VALUES (1, 1, 5.99), (2, 1, 2.50), (3, 1, 4.99), (4, 1, 5.50), (5, 1, 3.50), (6, 1, 3.49),
+  (7, 1, 5.99), (1, 2, 4.50);
+INSERT INTO `product` (name, stock_count) VALUES ('dog biscuits', 15),('flea powder', 20),('collar', 20),
+  ('nail file', 20),('vitamins', 20),('shampoo', 15),('tick remover', 10);
+INSERT INTO `card` VALUES ('1234123412341234','302010','VISA', '2028-7-04', '123'),
+  ('5678567856785678','112233', 'VISA', '2023-1-01', '999'),
+  ('1000100010001000','110033', 'MASTERCARD', '2025-12-12', '555');
+INSERT INTO `paypal` VALUES ('1111', 1000.00),
+  ('2222', 1000.00);
